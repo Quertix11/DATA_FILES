2661564c7e8bbb56e82c383a8ce43e949e7ff42a
Quertix11 <114050782+Quertix11@users.noreply.github.com>
2023-11-10 09:16:37 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+import flask
+from flask_peewee.peewee import Database  # Updated import
+
+DATABASE = {
+    'name': 'putnins.db',
+    'engine': 'peewee.SqliteDatabase'
+}
+DEBUG = True
+SECRET_KEY = 'noslepums'
+
+app = flask.Flask(__name__)
+app.config.from_object(__name__)
+
+db = Database(app)
+
+from putnins.routes import *
